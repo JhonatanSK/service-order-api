4a308a55c352f4d8a584e4a8220552e72eedde31
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-06-18 02:28:08 -0300

    feature: developing the domain system

---
+++ src/main/java/com/jhonatan/serviceorderapi/domain/exception/Exceptions.java
@@ -0,0 +1,11 @@
+package com.jhonatan.serviceorderapi.domain.exception;
+
+public class Exceptions extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public Exceptions(String message) {
+		super(message);
+	}
+
+}
---
+++ src/main/java/com/jhonatan/serviceorderapi/domain/repository/ClienteRepository.java
@@ -0,0 +1,18 @@
+package com.jhonatan.serviceorderapi.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jhonatan.serviceorderapi.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+	Cliente findByEmail(String email);
+	
+	
+}
