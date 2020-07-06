8515388374d2c356adc2addcbe275074e731f396
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-07-06 01:50:56 -0300

    feature: creating the EntityNotFOundException

---
+++ src/main/java/com/jhonatan/serviceorderapi/domain/exception/EntityNotFoundException.java
@@ -0,0 +1,12 @@
+package com.jhonatan.serviceorderapi.domain.exception;
+
+public class EntityNotFoundException extends Exceptions{
+
+	private static final long serialVersionUID = 1L;
+
+	public EntityNotFoundException(String message) {
+		super(message);
+	}
+
+
+}
